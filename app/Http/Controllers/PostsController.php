13cb836d97415af30b608df7d1d8e6d0c38848d5
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-06-27 21:16:28 +0300

    Update PostsController.php

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -73,7 +73,7 @@ class PostsController extends Controller
                 ], 403);
         }
 
-        $data->update($request->all();
+        $data->update($request->all());
         return $data;
     }
 
